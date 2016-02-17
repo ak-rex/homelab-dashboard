6686e15c312f02d63c219059810b7f23e6259ed7
rex <rex@linux.com>
2016-02-16 15:44:54 -0900

    testing

--- index.php
+++ index.php
@@ -218,6 +218,7 @@ function formatSize( $bytes )
               </div>
             </div>
             <div class="demo-separator mdl-cell--1-col"></div>
+            <div class="demo-options mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
               <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
                 <h2 class="mdl-card__title-text">Equipment</h2>
               </div>
