7242193d6b8a080122650c3ae0efa3738d47a6d3
rex <rex@linux.com>
2016-02-23 22:21:14 -0900

    complete for now

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HomeLab Dashboard</title>
+    <title>Homelab Dashboard</title>
 
     <!-- Add to homescreen for Chrome on Android -->
     <meta name="mobile-web-app-capable" content="yes">
@@ -46,7 +46,7 @@
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.1.1/material.cyan-light_blue.min.css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.1/material.green-red.min.css">
     <link rel="stylesheet" href="styles.css">
     <style>
     #view-source {
@@ -105,7 +105,7 @@ function formatSize( $bytes )
   padding: 2px 5px 2px 5px;
   margin: 0;
   display: inline;
-  background: #64dd17;
+  background: #2e7d32;
   color: white;
 }
 
@@ -113,25 +113,25 @@ function formatSize( $bytes )
   padding: 2px 5px 2px 5px;
   margin: 0;
   display: inline;
-  background: #d32f2f;
+  background: #c62828;
   color: white;
 }
 
 .progress {
-        border: 2px solid #0cdcff;
+        border: 2px solid #2e7d32;
         height: 32px;
         width: 100%;
         margin: 30px auto;
 }
 .progress .prgbar {
-        background: #0cdcff;
+        background: #2e7d32;
         width: <?php echo $dp; ?>%;
         position: relative;
         height: 32px;
         z-index: 999;
 }
 .progress .prgtext {
-        color: #888888;
+        color: #000000;
         text-align: center;
         font-size: 13px;
         padding: 9px 0 0;
@@ -146,7 +146,7 @@ function formatSize( $bytes )
 </style>
 
     <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-      <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
+      <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--black">
         <div class="mdl-layout__header-row">
           <span class="mdl-layout-title">Homelab Dashboard</span>
           <div class="mdl-layout-spacer"></div>
@@ -159,25 +159,27 @@ function formatSize( $bytes )
           </ul>
         </div>
       </header>
-      <div class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
+      <div class="demo-drawer mdl-layout__drawer mdl-color--grey-200 mdl-color-text--black">
         <header class="demo-drawer-header">
           <img src="images/user.jpg" class="demo-avatar">
-	<div class="demo-avatar-dropdown">
-	<div class="mdl-card__supporting-text mdl-color-text--white">
-            Homelab
-	</div>
+	<a class="mdl-color-text--white">
+            Homelab Dashboard
+	</a>
+	<a class="mdl-color-text--white">
+            Alaska, USA
+	</a>
             <div class="mdl-layout-spacer"></div>
-          </div>
         </header>
-        <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-700">
-          <a class="mdl-navigation__link" href="http://192.168.0.2" target="_blank"><i class="mdl-color-text--blue-grey-200 material-icons" role="presentation">storage</i>Unraid</a>
-          <a class="mdl-navigation__link" href="http://192.168.0.2:32400/web/index.html" target="_blank"><i class="mdl-color-text--blue-grey-200 material-icons" role="presentation">play_circle_filled</i>Plex</a>
-          <a class="mdl-navigation__link" href="http://192.168.0.2:8181" target="_blank"><i class="mdl-color-text--blue-grey-200 material-icons" role="presentation">visibility</i>PlexPy</a>
-          <a class="mdl-navigation__link" href="http://192.168.0.3:8112" target="_blank"><i class="mdl-color-text--blue-grey-200 material-icons" role="presentation">swap_vertical_circle</i>Deluge</a>
-          <a class="mdl-navigation__link" href="http://192.168.0.3:8989" target="_blank"><i class="mdl-color-text--blue-grey-200 material-icons" role="presentation">fiber_dvr</i>Sonarr</a>
-          <a class="mdl-navigation__link" href="http://192.168.0.3:5050" target="_blank"><i class="mdl-color-text--blue-grey-200 material-icons" role="presentation">weekend</i>CouchPotato</a>
+        <nav class="demo-navigation mdl-navigation mdl-color--grey-100">
+          <a class="mdl-navigation__link" href="http://192.168.0.2" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">storage</i>Unraid</a>
+          <a class="mdl-navigation__link" href="http://192.168.0.2:32400/web/index.html" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">play_circle_filled</i>Plex</a>
+          <a class="mdl-navigation__link" href="http://192.168.0.2:8181" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">visibility</i>PlexPy</a>
+          <a class="mdl-navigation__link" href="http://192.168.0.3:8112" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">swap_vertical_circle</i>Deluge</a>
+          <a class="mdl-navigation__link" href="http://192.168.0.3:8989" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">fiber_dvr</i>Sonarr</a>
+          <a class="mdl-navigation__link" href="http://192.168.0.3:5050" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">weekend</i>CouchPotato</a>
+          <a class="mdl-navigation__link" href="http://192.168.0.3:10000" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">settings</i>Webmin</a>
           <div class="mdl-layout-spacer"></div>
-          <a class="mdl-navigation__link" href="https://github.com/ak-rex/Dashboard" target="_blank"><i class="mdl-color-text--blue-grey-200 material-icons" role="presentation">help_outline</i><span class="visuallyhidden">Help</span></a>
+          <a class="mdl-navigation__link" href="https://github.com/ak-rex/Dashboard" target="_blank"><i class="mdl-color-text--black material-icons" role="presentation">help_outline</i><span class="visuallyhidden">Help</span></a>
         </nav>
       </div>
       <main class="mdl-layout__content mdl-color--grey-100">
@@ -188,7 +190,7 @@ function formatSize( $bytes )
 		</div>
       		  <div class='prgbar'></div>
       		  <div class='prginfo'>
-		<div class="mdl-card__supporting-text mdl-color-text--grey-600">
+		<div class="mdl-card__supporting-text mdl-color-text--black">
 		<?php echo "$du Used - $df Free - $dt Total"; ?></div>
                 <span style='clear: both;'></span>
 	</div>
@@ -201,93 +203,45 @@ function formatSize( $bytes )
 	     <img src="http://xxxx:xxxx@192.168.0.7/PSIA/Streaming/channels/201/picture" width="49%"></center>
             <div class="demo-separator mdl-cell--1-col"></div>
 	    <center class="pattern">
-	     <img src="http://xxxx:xxxx@192.168.0.7/PSIA/Streaming/channels/201/picture" width="49%">
+	     <img src="http://xxxx:xxxx@192.168.0.7/PSIA/Streaming/channels/301/picture" width="49%">
 	     <img src="images/security.jpg" width="49%"></center>
             <div class="demo-separator mdl-cell--1-col"></div>
           </div>
           <div class="demo-cards mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-grid mdl-grid--no-spacing">
             <div class="demo-updates mdl-card mdl-shadow--8dp mdl-cell mdl-cell--4-col mdl-cell--3-col-tablet mdl-cell--12-col-desktop">
-              <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
+              <div class="mdl-card__title mdl-card--expand mdl-color--green-800">
                 <h2 class="mdl-card__title-text">Services</h2>
               </div>
-              <div class="mdl-card__supporting-text mdl-color-text--grey-600">
-		 <div>Storage Server-</div>
-                 <div>UnRaid:		<?php echo GetServerStatus('192.168.0.2',80); ?></div>
-		 <div>Plex:		<?php echo GetServerStatus('192.168.0.2',32400); ?></div>
-		 <div>PlexPy:		<?php echo GetServerStatus('192.168.0.2',8181); ?></div>
-		 <div>SeedBox-</div>
-                 <div>Deluge:		<?php echo GetServerStatus('192.168.0.3',8112); ?></div>
-		 <div>Sonarr:		<?php echo GetServerStatus('192.168.0.3',8989); ?></div>
-		 <div>CouchPotato:	<?php echo GetServerStatus('192.168.0.3',5050); ?></div>
+              <div class="mdl-card__supporting-text mdl-color-text--black">
+		 <div>Storage Server:</div>
+                 <div>UnRaid -		<?php echo GetServerStatus('192.168.0.2',80); ?></div>
+		 <div>Plex -		<?php echo GetServerStatus('192.168.0.2',32400); ?></div>
+		 <div>PlexPy -		<?php echo GetServerStatus('192.168.0.2',8181); ?></div>
+		 <div>SeedBox:</div>
+                 <div>Deluge -		<?php echo GetServerStatus('192.168.0.3',8112); ?></div>
+		 <div>Sonarr -		<?php echo GetServerStatus('192.168.0.3',8989); ?></div>
+		 <div>CouchPotato -	<?php echo GetServerStatus('192.168.0.3',5050); ?></div>
               </div>
             </div>
             <div class="demo-separator mdl-cell--1-col"></div>
             <div class="demo-updates mdl-card mdl-shadow--8dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
-              <div class="mdl-card__title mdl-card--expand mdl-color--deep-purple-500">
+              <div class="mdl-card__title mdl-card--expand mdl-color--red-800">
                 <h2 class="mdl-card__title-text">Equipment</h2>
               </div>
-              <div class="mdl-card__supporting-text mdl-color-text--grey-600">
-		 <div>Network-</div>
-                 <div>Router:		<?php echo GetServerStatus('192.168.0.1',80); ?></div>
-		 <div>Wireless AP:	<?php echo GetServerStatus('192.168.0.100',80); ?></div>
-		 <div>Appliances-</div>
-                 <div>Printer:		<?php echo GetServerStatus('192.168.0.5',80); ?></div>
-                 <div>Surveillance:	<?php echo GetServerStatus('192.168.0.7',80); ?></div>
+              <div class="mdl-card__supporting-text mdl-color-text--black">
+		 <div>Network:</div>
+                 <div>Router -		<?php echo GetServerStatus('192.168.0.1',80); ?></div>
+		 <div>Wireless AP -	<?php echo GetServerStatus('192.168.0.100',80); ?></div>
+		 <div>Appliances:</div>
+                 <div>Printer -		<?php echo GetServerStatus('192.168.0.5',80); ?></div>
+                 <div>Surveillance -	<?php echo GetServerStatus('192.168.0.7',80); ?></div>
               </div>
             </div>
-
           </div>
         </div>
       </main>
     </div>
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" style="position: fixed; left: -1000px; height: -1000px;">
-        <defs>
-          <mask id="piemask" maskContentUnits="objectBoundingBox">
-            <circle cx=0.5 cy=0.5 r=0.49 fill="white" />
-            <circle cx=0.5 cy=0.5 r=0.40 fill="black" />
-          </mask>
-          <g id="piechart">
-            <circle cx=0.5 cy=0.5 r=0.5 />
-            <path d="M 0.5 0.5 0.5 0 A 0.5 0.5 0 0 1 0.95 0.28 z" stroke="none" fill="rgba(255, 255, 255, 0.75)" />
-          </g>
-        </defs>
-      </svg>
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 250" style="position: fixed; left: -1000px; height: -1000px;">
-        <defs>
-          <g id="chart">
-            <g id="Gridlines">
-              <line fill="#888888" stroke="#888888" stroke-miterlimit="10" x1="0" y1="27.3" x2="468.3" y2="27.3" />
-              <line fill="#888888" stroke="#888888" stroke-miterlimit="10" x1="0" y1="66.7" x2="468.3" y2="66.7" />
-              <line fill="#888888" stroke="#888888" stroke-miterlimit="10" x1="0" y1="105.3" x2="468.3" y2="105.3" />
-              <line fill="#888888" stroke="#888888" stroke-miterlimit="10" x1="0" y1="144.7" x2="468.3" y2="144.7" />
-              <line fill="#888888" stroke="#888888" stroke-miterlimit="10" x1="0" y1="184.3" x2="468.3" y2="184.3" />
-            </g>
-            <g id="Numbers">
-              <text transform="matrix(1 0 0 1 485 29.3333)" fill="#888888" font-family="'Roboto'" font-size="9">500</text>
-              <text transform="matrix(1 0 0 1 485 69)" fill="#888888" font-family="'Roboto'" font-size="9">400</text>
-              <text transform="matrix(1 0 0 1 485 109.3333)" fill="#888888" font-family="'Roboto'" font-size="9">300</text>
-              <text transform="matrix(1 0 0 1 485 149)" fill="#888888" font-family="'Roboto'" font-size="9">200</text>
-              <text transform="matrix(1 0 0 1 485 188.3333)" fill="#888888" font-family="'Roboto'" font-size="9">100</text>
-              <text transform="matrix(1 0 0 1 0 249.0003)" fill="#888888" font-family="'Roboto'" font-size="9">1</text>
-              <text transform="matrix(1 0 0 1 78 249.0003)" fill="#888888" font-family="'Roboto'" font-size="9">2</text>
-              <text transform="matrix(1 0 0 1 154.6667 249.0003)" fill="#888888" font-family="'Roboto'" font-size="9">3</text>
-              <text transform="matrix(1 0 0 1 232.1667 249.0003)" fill="#888888" font-family="'Roboto'" font-size="9">4</text>
-              <text transform="matrix(1 0 0 1 309 249.0003)" fill="#888888" font-family="'Roboto'" font-size="9">5</text>
-              <text transform="matrix(1 0 0 1 386.6667 249.0003)" fill="#888888" font-family="'Roboto'" font-size="9">6</text>
-              <text transform="matrix(1 0 0 1 464.3333 249.0003)" fill="#888888" font-family="'Roboto'" font-size="9">7</text>
-            </g>
-            <g id="Layer_5">
-              <polygon opacity="0.36" stroke-miterlimit="10" points="0,223.3 48,138.5 154.7,169 211,88.5
-              294.5,80.5 380,165.2 437,75.5 469.5,223.3 	"/>
-            </g>
-            <g id="Layer_4">
-              <polygon stroke-miterlimit="10" points="469.3,222.7 1,222.7 48.7,166.7 155.7,188.3 212,132.7
-              296.7,128 380.7,184.3 436.7,125 	"/>
-            </g>
-          </g>
-        </defs>
-      </svg>
-      <a href="https://google.com.com" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color-text--white">Google</a>
+      <a href="http://admin:1960@192.168.0.7/PSIA/Streaming/channels/201/picture" target="_blank" id="view-source" class="mdl-button--fab mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color-text--white"><i class="material-icons">add</i></a>
     <script src="https://code.getmdl.io/1.1.1/material.min.js"></script>
   </body>
 </html>
